032285458df8fd3189d3c12bd0dbebbd7081e0c0
nachitosoria <nacho.soria.casella@gmail.com>
2020-07-15 15:34:34 -0300

    algunos detalles de vistas

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
-    "database": "base_ulimited_db",
+    "password": "root",
+    "database": "base_unlimited_db",
+    "port": 8889,
     "host": "127.0.0.1",
     "dialect": "mysql",
     "define":{
